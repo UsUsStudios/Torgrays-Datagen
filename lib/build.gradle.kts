35459aea02cc8216ccc1774cdef3b0e786c837f3
UsUsStudios <biruni056@gmail.com>
2025-05-03 14:54:00 -0600

    Initial commit, Java library

---
+++ lib/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    `java-library`
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(libs.junit.jupiter)
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(23)
+    }
+}
