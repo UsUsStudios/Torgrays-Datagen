aad5d6f4fdd6854fac1a96abd63584f1a0b936d3
UsUsStudios <biruni056@gmail.com>
2025-05-07 17:39:47 -0600

    Javadoc for Entity

--- lib/src/main/java/com/ususstudios/torgrays_datagen/generators/Entity.java
+++ lib/src/main/java/com/ususstudios/torgrays_datagen/generators/Entity.java
@@ -1,11 +1,27 @@
 package com.ususstudios.torgrays_datagen.generators;
 
+/**
+ * A record for entities, used in the map generator.
+ * @param path The path to the entity class.
+ * @param col The column where the entity spawns.
+ * @param row The row where the entity spawns.
+ * @param lootTable The name of the loot table that should be used when the entity is killed/opened/used, etc. Can be
+ *                 null if there is no loot table associated.
+ */
 public record Entity(
 	String path,
 	int col,
 	int row,
 	String lootTable
 ) {
+	/**
+	 * A record for entities, used in the map generator.
+	 * @param entityClass The entity class, to get the path.
+	 * @param col The column where the entity spawns.
+	 * @param row The row where the entity spawns.
+	 * @param lootTable The name of the loot table that should be used when the entity is killed/opened/used, etc. Can
+	 *                     be null if there is no loot table associated.
+	 */
 	public Entity(
 			Class<?> entityClass,
 			int col,
