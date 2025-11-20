2ff07ed78333df6ac1c7d4445ad08df92320f401
UsUsStudios <biruni056@gmail.com>
2025-11-19 18:28:41 -0700

    1.2.1

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -22,7 +22,7 @@ publishing {
         create<MavenPublication>("jitpack") {
             groupId = "com.github.UsUsStudios"
             artifactId = "Torgrays-Datagen"
-            version = "v1.2"
+            version = "v1.2.1"
 
             from(components["java"])
         }
